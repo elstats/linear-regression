52d07dd7c8015754a496da3cf06f24071d73027f
Michał Jezierski <ambinanitelo@gmail.com>
2018-02-06 10:56:35 +0100

    eslint

--- test.js
+++ test.js
@@ -1,5 +1,20 @@
 import test from 'ava';
+import linearRegression from './';
 
 test('should pass', t => {
-    t.pass();
+    const given = [
+        [43, 99],
+        [21, 65],
+        [25, 79],
+        [42, 75],
+        [57, 87],
+        [59, 81]
+    ];
+
+    const expected = linearRegression(given);
+
+    t.is(expected, {
+        a: 65.1415715245131,
+        b: 0.38522498321020804
+    });
 });
