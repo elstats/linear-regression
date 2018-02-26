aaaf3f341076dc8e61d0d5947c7362f131129384
Michał Jezierski <ambinanitelo@gmail.com>
2018-02-26 20:34:47 +0100

    r-squared

--- index.js
+++ index.js
@@ -5,9 +5,24 @@ import covariance from '@elstats/covariance';
 export default function linearRegression(data) {
     const X = data.map(x => x[0]);
     const Y = data.map(x => x[1]);
+    const meanY = mean(Y);
 
     const a = covariance(X, Y) / variance(X);
-    const b = mean(Y) - a * mean(X);
+    const b = meanY - a * mean(X);
 
-    return { a, b };
+    const totalSumOfSquares = Y.reduce((acc, cur) => {
+        acc += Math.pow(cur - meanY, 2);
+        return acc;
+    }, 0);
+
+    const residualSumOfSquares = Y.reduce((acc, cur, i) => {
+        acc += Math.pow(cur - (a * X[i] + b), 2);
+        return acc;
+    }, 0);
+
+    return {
+        a,
+        b,
+        rSquared: 1 - residualSumOfSquares / totalSumOfSquares
+    };
 }
--- test.js
+++ test.js
@@ -1,20 +1,21 @@
 import test from 'ava';
-import linearRegression from './';
+import linearRegression from './index';
 
 test('should calculate linear regression', t => {
     const given = [
-        [43, 99],
-        [21, 65],
-        [25, 79],
-        [42, 75],
-        [57, 87],
-        [59, 81]
+        [1,   0.5],
+        [2,   1.1],
+        [3.1, 1.7],
+        [3.9, 1.98],
+        [5.2, 2.45],
+        [6,   3]
     ];
 
     const expected = linearRegression(given);
 
     t.deepEqual(expected, {
-        a: 0.38522498321020804,
-        b: 65.1415715245131
+        a: 0.4745265503156331,
+        b: 0.1116728555514297,
+        rSquared: 0.9898548897522457
     });
 });
